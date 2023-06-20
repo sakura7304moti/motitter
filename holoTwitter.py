0fb4d531418abd0cc70ef5ba1cc6f287d6731dd9
DESKTOP-41T63M9\sakur <sakura7304moti@gmail.com>
2023-06-20 17:52:26 +0900

    schedulerとholoTwitter修正

--- holoTwitter.py
+++ holoTwitter.py
@@ -2,8 +2,8 @@ from src import moti
 from src import utils
 
 try:
-    model = moti.Downloader()
-    model.holo_downloader()
+    model = moti.Getter()
+    model.holo_getter(14, 10000)
 except Exception as e:
     print(e)
     utils.message(e)
